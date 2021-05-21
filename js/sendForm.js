57931b8bab43c76c3abdd13ca2683e93ee4f4db5
andriy-lanyk <LAM1988@ukr.net>
2021-05-21 11:26:02 +0300

    Начал делать возможность отправки формы

---
+++ js/sendForm.js
@@ -0,0 +1,6 @@
+const server = 'https://jsonplaceholder.typicode.com/posts';
+
+const sendData = (data, callBack, falseCallBack) => {
+    const request = new XMLHttpRequest();
+    request.open('POST', server);
+}
